92a0beea8debad321b7ea4a00f4a5a9db0d44f11
Dell ProSupport GSE <79069476+DellProSupportGse@users.noreply.github.com>
2024-05-01 17:13:14 -0500

    Update 1_HCI_Switchless_NetATC_Node_Config.ps1

--- 1_HCI_Switchless_NetATC_Node_Config.ps1
+++ 1_HCI_Switchless_NetATC_Node_Config.ps1
@@ -1,5 +1,5 @@
 ﻿#Configure Non-Converged Switchless with Network ATC
-#Version v1.1.1
+#Version v1.1.2
 #Varables
     $NodeName="AzHCI1"
         
@@ -52,7 +52,10 @@
     }
     If((Get-NetAdapter $S1Nic,$S2Nic | Select InterfaceDescription) -imatch "E810"){
         Get-NetAdapter $S1Nic,$S2Nic | Set-NetAdapterAdvancedProperty -DisplayName "Receive Buffers*" -DisplayValue 4096 -Confirm:$false
-        Get-NetAdapter $S1Nic,$S2Nic | Set-NetAdapterAdvancedProperty -DisplayName "Send Buffers*" -DisplayValue 4096 -Confirm:$false
+	    #Added to set Send or Transmit Buffer
+        IF(Get-NetAdapterAdvancedProperty $S1Nic,$S2Nic -DisplayName "Send Buffers*" -ErrorAction SilentlyContinue){
+		Get-NetAdapter $S1Nic,$S2Nic | Set-NetAdapterAdvancedProperty -DisplayName "Send Buffers*" -DisplayValue 4096 -Confirm:$false}
+  	    else{Get-NetAdapter $S1Nic,$S2Nic | Set-NetAdapterAdvancedProperty -DisplayName "Transmit Buffers*" -DisplayValue 512 -Confirm:$false}
     }    
 
 # Exclude iDRAC NIC from Cluster
